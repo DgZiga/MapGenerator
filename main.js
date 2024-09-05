0027164abfec809717347dcd9dc8cf3f78217106
avon <dgziga@gmail.com>
2024-09-05 10:23:08 +0200

    First commit: working stub of wfc

---
+++ main.js
@@ -0,0 +1,132 @@
+var input_probs = IVAN_PROBS;
+const OUTPUT_W = 20;
+const OUTPUT_H = 20;
+
+var output = new Array();
+var output_probs = new Array(); //output is a 3d array: first two indexes are position (x/y), each position contains a list of possibilities
+var prob_calced_ctr = new Array();
+
+//init output array
+var probs_tmpl = new Array();
+for(var i=0; i<Object.keys(input_probs).length; i++){
+    probs_tmpl[i]=i;
+}
+
+for(var i=0; i<OUTPUT_W; i++){
+    output_probs[i] = new Array();
+    output[i] = new Array();
+    prob_calced_ctr[i] = new Array();
+    for(var j=0; j<OUTPUT_H; j++){
+        output_probs[i][j] = probs_tmpl.slice(); //clone array
+        output[i][j] = -1;
+        prob_calced_ctr[i][j] = false;
+    }
+}
+
+function find_lowest_entropy_cell(){
+    var lowestI = -1;
+    var lowestJ = -1;
+    var lowestCnt = probs_tmpl.length + 1*1;
+    for(var i=0; i<OUTPUT_W; i++){
+        for(var j=0; j<OUTPUT_H; j++){
+            if(output[i][j] == -1 && output_probs[i][j].length<lowestCnt){
+                lowestCnt = output_probs[i][j].length;
+                var lowestI = i;
+                var lowestJ = j;
+            }
+        }
+    }
+    return [lowestI, lowestJ];
+}
+
+function recalc_prob(x, y){
+    if(x<0 || y<0 || x==OUTPUT_W || y==OUTPUT_H || prob_calced_ctr[x][y]){
+        return
+    }
+    prob_calced_ctr[x][y] = true;
+    if(output[x][y] != -1){
+        recalc_prob(x-1, y  )
+        recalc_prob(x+1, y  )
+        recalc_prob(x  , y-1)
+        recalc_prob(x  , y+1)
+        return;
+    }
+    var l = new Array();
+    var r = new Array();
+    var u = new Array();
+    var d = new Array();
+    if(x>0)          {l=output_probs[x-1][y  ]}
+    if(x<OUTPUT_W-1) {r=output_probs[x+1][y  ]}
+    if(y>0)          {u=output_probs[x  ][y-1]}
+    if(y<OUTPUT_H-1) {d=output_probs[x  ][y+1]}
+
+    var l_allowed_tiles = new Array();
+    for(tile of l){
+        l_allowed_tiles = l_allowed_tiles.concat(input_probs[""+tile].right)
+    }
+    var r_allowed_tiles = new Array();
+    for(tile of r){
+        r_allowed_tiles = r_allowed_tiles.concat(input_probs[""+tile].left)
+    }
+    var u_allowed_tiles = new Array();
+    for(tile of u){
+        u_allowed_tiles = u_allowed_tiles.concat(input_probs[""+tile].down)
+    }
+    var d_allowed_tiles = new Array();
+    for(tile of d){
+        d_allowed_tiles = d_allowed_tiles.concat(input_probs[""+tile].up)
+    }
+    if(l.length == 0){l_allowed_tiles = probs_tmpl.slice()}
+    if(r.length == 0){r_allowed_tiles = probs_tmpl.slice()}
+    if(u.length == 0){u_allowed_tiles = probs_tmpl.slice()}
+    if(d.length == 0){d_allowed_tiles = probs_tmpl.slice()}
+
+    output_probs[x][y] = output_probs[x][y].filter(e => d_allowed_tiles.includes(e) && u_allowed_tiles.includes(e) && l_allowed_tiles.includes(e) && r_allowed_tiles.includes(e));
+    if(output_probs[x][y].length != probs_tmpl.length){
+        recalc_prob(x-1, y  )
+        recalc_prob(x+1, y  )
+        recalc_prob(x  , y-1)
+        recalc_prob(x  , y+1)
+    }
+
+}   
+
+function observe(x, y){
+    var probs = output_probs[x][y]
+    var i = Math.floor(Math.random() * probs.length);
+    output[x][y] = output_probs[x][y][i]
+    output_probs[x][y] = [output[x][y]];
+
+    //propagate changes to nearby nodes
+    recalc_prob(x, y)
+    for(var i=0; i<OUTPUT_W; i++){
+        for(var j=0; j<OUTPUT_H; j++){
+            prob_calced_ctr[i][j] = false;
+        }
+    }
+    
+}
+
+function start(){
+    var arr = find_lowest_entropy_cell();
+    var x = arr[0];
+    var y = arr[1];
+    var i=0;
+    do {
+        observe(x,y);
+        arr = find_lowest_entropy_cell();
+        x = arr[0];
+        y = arr[1];
+        i++;
+    } while(i<20)//x != -1 && y!= -1)
+    console.log("done")
+    var html = ""
+    $("#continer")[0].style.width=OUTPUT_W*16+"px"
+    for(var i=0; i<OUTPUT_W; i++){
+        for(var j=0; j<OUTPUT_H; j++){
+            html += '<img src="img/provaIvan/tile'+output[j][i]+'.png" />'
+        }
+    }
+    $("#continer")[0].innerHTML=html
+
+}
---
+++ pythonThingy/main.py
@@ -0,0 +1,174 @@
+from PIL import Image
+from PIL import ImageChops
+import json
+import math
+import pdb
+
+im = Image.open(r"img/provaIvan/ivan.png")
+
+
+
+rows=29
+cols=27
+tileSize=16 #tile is 16x16
+
+seenImages = []
+tileIdCounter=0
+map = []
+
+class Tile:
+    def __init__(self, id, img):
+        self.id=id
+        self.img=img
+
+i=0
+j=0
+while i<rows:
+    map.append([]) #create row
+    while j<cols:
+        im1 = im.crop((j*tileSize, i*tileSize, (j+1)*tileSize, (i+1)*tileSize)) #crop to a tile
+
+        tileId=-1
+        seen=0
+        for seenImage in seenImages: #if the tile has already been seen, just reuse the same tileid
+            if list(im1.getdata()) == list(seenImage.img.getdata()):
+                seen=1
+                tileId=seenImage.id
+
+        if seen != 1: #if the tile has yet to be seen, create the img, give it an id, and append it to the seen ones
+            im1.save("img/provaIvan/tile"+str(tileIdCounter)+".png")
+            tile= Tile(tileIdCounter, im1)
+            seenImages.append(tile)
+            tileId = tileIdCounter
+            tileIdCounter+=1
+
+        map[i].append(tileId)
+
+        j+=1
+    i+=1
+    j=0
+
+
+#"map" now contains a 2d array where each position is a tile id
+
+
+class TileRelationship:
+    def __init__(self):
+        self.up   =[]
+        self.down =[]
+        self.left =[]
+        self.right=[]
+
+tileRelationships = {}
+tileId=0
+while tileId<tileIdCounter:#for each tile id
+    i=0
+    j=0
+    while i<rows:
+        while j<cols:
+
+            if map[i][j]==tileId:
+                rel=tileRelationships.get(tileId, "")
+                if rel == "":
+                    tileRelationships[tileId] = TileRelationship()
+                    rel = tileRelationships[tileId]
+                
+                if i>0:
+                    rel.up.append(map[i-1][j])
+                if i<rows-1:
+                    rel.down.append(map[i+1][j])
+                if j>0:
+                    rel.left.append(map[i][j-1])
+                if j<cols-1:
+                    rel.right.append(map[i][j+1])
+
+            j+=1
+
+        i+=1
+        j=0
+
+    tileId+=1
+
+#print(map)
+#print()
+#print()
+#print()
+print(json.dumps(tileRelationships, default=lambda x: x.__dict__))
+#print()
+#print()
+#print()
+#print()
+
+#tileRelationships dict now may contain multiple entries of the same tile for each id, the last step is to convert them into bitmaps
+
+powersOf2 = {}
+iterator = range(tileIdCounter+1)
+for i in iterator:
+    powersOf2[i] = int(math.pow(2,i))
+    
+print(json.dumps(powersOf2, default=lambda x: x.__dict__))
+
+# doesnt work: python doesnt know how to handle big numbers, although it says it does:
+#  >>> 39969865730104624 + int(math.pow(2,62))
+#  4651655884157492224
+#  >>> 39969865730104624 + int(4611686018427387904)
+#  4651655884157492528
+#  >>> int(math.pow(2,62))
+#  4611686018427387904
+def normalizeProbabilityList(input, log=0):
+    if(log != 0):
+        print("Start bitmap population")
+    bitmap = int(0)
+    for entry in input:
+        p = 2**entry #powersOf2[entry]     #int(math.pow(2,entry))
+        old = bitmap
+        bitmap |= 2**entry
+        if(log != 0):
+            if(old != bitmap):
+                print('{:.0f}'.format(old) +" | "+'{:.0f}'.format(p) +" = "+ '{:.0f}'.format(bitmap))
+            print("bitmap "+'{:.0f}'.format(bitmap)+" augmented with "+'{:.0f}'.format(p)+", derived from: "+str(entry)+", bitmap: "+"{0:b}".format(bitmap))
+
+    return bitmap
+
+
+def univoci(input):
+    output = []
+    for entry in input:
+        if(entry in output):
+            continue
+        output.append(entry)
+    return output
+
+
+tileId=0
+while tileId<tileIdCounter: #for each tile id
+    rel=tileRelationships[tileId]
+    #commentati, visto che python non sa gestirsi i numeri
+    #rel.up    = '{:.0f}'.format(normalizeProbabilityList(rel.up))
+    #rel.down  = '{:.0f}'.format(normalizeProbabilityList(rel.down))
+    #rel.left  = '{:.0f}'.format(normalizeProbabilityList(rel.left, 1 if tileId==5 else 0))
+    #rel.right = '{:.0f}'.format(normalizeProbabilityList(rel.right))
+    rel.up    = univoci(rel.up)
+    rel.down  = univoci(rel.down)
+    rel.left  = univoci(rel.left)
+    rel.right = univoci(rel.right)
+    tileId+=1
+
+print()
+print()
+print()
+print(json.dumps(tileRelationships, default=lambda x: x.__dict__))
+
+"""
+tile Adjacencies are defined as a n-bit number (bitmap), where each bit corresponds to a tileset, subtracting becomes as simple as a bitwise NAND, adding is bitwise OR.
+Because they have to be serialized into JSON, they will be represented as Strings 
+
+{
+    tileId:{
+        "up":"bitmap",
+        "down":"bitmap",
+        "right":"bitmap",
+        "left":"bitmap"
+    }
+}
+"""
