59065300c8f8ac36736ff3d08ce484b8de02d859
avon <dgziga@gmail.com>
2024-11-20 19:56:18 +0100

    Implemented Vectorial space for walkable algorithm

--- walkable.js
+++ walkable.js
@@ -11,19 +11,20 @@ var walkable_non_warp = WFC.s_input_walkable.filter(e=>{
     }) == -1 ? true : false
 })
 
-
-function paint_walkables(wfc) {
+var paintedCoords = new Array()
+function setPaintedCoords(coords){
+    paintedCoords=paintedCoords.concat(coords)
+}
+async function paint_walkables(wfc) {
     for(var i=0; i<WFC.OUTPUT_W; i++){
         for(var j=0; j<WFC.OUTPUT_H; j++){
-            var isTileWalkable = walkables_coords.findIndex(e=>{return e.x===i && e.y===j}) == -1 ? false : true;
-            if(!isTileWalkable){
-                wfc.set_superposition_no_recalc(i, j, non_walkable_superpos) //tilesets outside walkable path can also be walkable. This somehow fixes issues for my tileset. More research needed
-            }
+            wfc.set_superposition_no_recalc(i, j, non_walkable_superpos) //tilesets outside walkable path cannot be walkable. More research needed
         }
     }
-    for(coords of walkables_coords){
+    const brushSize = 4;
+    for(var vector of walkables_coords){
         //console.log("Setting walkable "+coords.x + ", " +coords.y);
-        wfc.set_superposition_no_recalc(coords.x,coords.y,walkable_non_warp)
+        vector.rasterize(wfc.output_probs, new Brush(tile_ids_to_bitmap(walkable_non_warp), brushSize))
     }
     
     // Print graph
@@ -42,7 +43,7 @@ function paint_walkables(wfc) {
             for(var j=0; j<WFC.OUTPUT_H; j++){
                 //horrible but idc
                 var isWalkable = false
-                for(coords of walkables_coords){
+                for(coords of paintedCoords){
                     if(j == coords.x && i == coords.y){
                         isWalkable=true;
                     }
@@ -58,9 +59,21 @@ function paint_walkables(wfc) {
         console.log(printstr)
     }
     showDebugGraph(); //comment this out if you want
+    
+    var delay = function(ms) {
+        return new Promise(resolve => setTimeout(resolve, ms));
+    }
+    //debug stuff
+    wfc.debug=true
+    renderOutput(wfc);
+    await delay(1000)
+    debugger;
 
     wfc.start_recalc_prob(fixed_end_coords.x, fixed_end_coords.y)
 
+    renderOutput(wfc);
+    await delay(1000)
+    debugger;
     
     var lowest_entropy_coord = wfc.find_lowest_entropy_cell()
     var lex = lowest_entropy_coord[0]
@@ -84,6 +97,12 @@ function randomize_walkable_coords(){
         walkables_coords = walkables_coords.concat(dumb_algorithm(fixed_end_coords));
     }
     
+    //scale x0 y0 to matrix coords
+    var mx = Math.ceil(fixed_end_coords.x*WFC.OUTPUT_W/100)-1
+    var my = Math.ceil(fixed_end_coords.y*WFC.OUTPUT_H/100)-1
+    if(mx<0){mx = 0}
+    if(my<0){my = 0}
+    fixed_end_coords = new Coord(mx, my)    
 }
 
 
@@ -96,72 +115,21 @@ var walk_mod=[ //indexed by dir. 0=UP, 1=DOWN, 2=LEFT, 3=RIGHT
 var dir_name = ["up", "down", "left", "right"]
 
 function dumb_algorithm(fixed_end_coords = undefined){
-    var out = []
     var start_coords = random_edge_coords()
     var end_coords   = fixed_end_coords === undefined ? random_edge_coords() : fixed_end_coords
-    var curr_x = start_coords.x;
-    var curr_y = start_coords.y;
-    //PIck a random direction and run towards it for variable_len times. Repeat until on target square 
-    while(curr_x != end_coords.x || curr_y != end_coords.y){
-        out = out.concat(new Coord(curr_x-1, curr_y-1))
-        out = out.concat(new Coord(curr_x  , curr_y-1))
-        out = out.concat(new Coord(curr_x+1, curr_y-1))
-        out = out.concat(new Coord(curr_x-1, curr_y  ))
-        out = out.concat(new Coord(curr_x  , curr_y  ))
-        out = out.concat(new Coord(curr_x+1, curr_y  ))
-        out = out.concat(new Coord(curr_x-1, curr_y+1))
-        out = out.concat(new Coord(curr_x  , curr_y+1))
-        out = out.concat(new Coord(curr_x+1, curr_y+1))
-        var dir = shortest_dir(new Coord(curr_x, curr_y), end_coords);//randomInt(4); //value between 0-3. 0=UP, 1=DOWN, 2=LEFT, 3=RIGHT
-        if(randomInt(5) == -1){ // with 1/5 chance just pick a random direction
-            var dir = randomInt(4); //value between 0-3. 0=UP, 1=DOWN, 2=LEFT, 3=RIGHT
-        }
-
-        //console.log("picked "+dir_name[dir]+" direction")
-        var variable_len=randomInt(3)*1+1*1;
-        for(var i =0 ; i<variable_len; i++){
-            out = out.concat(new Coord(curr_x, curr_y))
-            curr_x += walk_mod[dir].x;
-            curr_y += walk_mod[dir].y;
-            if(curr_x < 0){ curr_x = 0;}
-            if(curr_y < 0){ curr_y = 0;}
-            if(curr_x >= WFC.OUTPUT_W){ curr_x = WFC.OUTPUT_W-1;}
-            if(curr_y >= WFC.OUTPUT_H){ curr_y = WFC.OUTPUT_H-1;}
-
-            if(curr_x == end_coords.x && curr_y == end_coords.y){
-                break;
-            }
-        }
-    }
-
-    out = out.filter((value, index, array) => array.indexOf(value) === index); //remove duplicates
-    out = out.filter((value, index, array) => value.x >= 0 && value.y >= 0 && value.x <= WFC.OUTPUT_W-1 && value.y <= WFC.OUTPUT_H-1); //remove invalid coords
-
-    return out
+    return new Vector(start_coords.x, start_coords.y, end_coords.x, end_coords.y);
 }
 
+//returns Coord object with random coordinates in Vector space (1-100)
 function random_edge_coords(){
-    var random_coords = new Coord(randomInt(WFC.OUTPUT_W), randomInt(WFC.OUTPUT_H))
+    var random_coords = new Coord(randomInt(100)+1, randomInt(100)+1)
     var which_fixed = randomInt(2); //0 = x coord is fixed, 1 = y coord is fixed
     var min_or_max  = randomInt(2); //0 = fixed coord will be set to min, 1 = fixed coord will be set to max
 
     if(which_fixed == 0){
-        return new Coord(min_or_max == 0 ? 0 : WFC.OUTPUT_W-1, random_coords.y)
+        return new Coord(min_or_max == 0 ? 0 : 100, random_coords.y)
     } else {
-        return new Coord(random_coords.x                     , min_or_max == 0 ? 0 : WFC.OUTPUT_H-1)
-    }
-}
-
-function shortest_dir(from, to){
-    var x_diff     = Math.abs(to.x - from.x);
-    var y_diff     = Math.abs(to.y - from.y);
-    var x_diff_neg = from.x > to.x;
-    var y_diff_neg = from.y > to.y;
-
-    if(x_diff > y_diff){
-        return x_diff_neg ? 2 : 3
-    } else {
-        return y_diff_neg ? 0 : 1
+        return new Coord(random_coords.x          , min_or_max == 0 ? 0 : 100)
     }
 }
 
---
+++ walkableAlg.js
@@ -0,0 +1,168 @@
+/* This implementation uses two coordinate systems: Vector space and Matrix space
+ * Vector space has possible coordinates going from 1 to 100. It is used for vectorial objects such as Vectors and Bezier Curves
+ * Matrix space is basically the WFC input matrix. 
+ * The code scales and overlaps Vector space objects to the Matrix space, and changes the Matrix space accordingly. This process is called "Rasterization"
+ * 
+ * A Brush defines how the Matrix space is changed, in particular it defines:
+ * - the superposition that all Matrix cells must be set to if any Vector object overlaps them
+ * - the width of the stroke
+ * - an optional softness option, which will smooth the edges
+ */
+class Brush{
+    constructor(superposition, width, brushSoftness=new BrushSoftness(0,0)){
+        this.superposition= superposition
+        this.width        = width
+        this.brushSoftness= brushSoftness
+    }
+
+    paint(matrix, x, y){
+        const matrixW = matrix.length
+        const matrixY = matrix[0].length
+
+        //draw a square
+        var extraWidth = this.width-1 
+        var startX = x-extraWidth
+        var startY = y-extraWidth
+        var endX = x+extraWidth
+        var endY = y+extraWidth
+
+        if(startX<0){startX = 0}
+        if(startY<0){startY = 0}
+        if(endX>=matrixW){endX = matrixW-1}
+        if(endY>=matrixY){endY = matrixY-1}
+
+        for(var i=startX; i<=endX; i++){
+            for(var j=startY; j<=endY; j++){
+                matrix[i][j] = this.superposition
+                setPaintedCoords(new Coord(i, j))
+            }
+        }
+
+        //TODO: implement softness :)
+    }
+}
+class BrushSoftness{
+    constructor(superposition, width){
+        this.superposition= superposition
+        this.width        = width
+    }
+}
+
+const { abs, sign } = Math;
+//Vector coordinates are expressed in a 1-100 coordinate system that has to be translated to the wfc matrix
+class Vector{
+    constructor(fromX, fromY, toX, toY){
+        this.from= new Coord(fromX, fromY)
+        this.to  = new Coord(toX  , toY)
+    }
+
+    rasterize(matrix, brush){
+        const matrixW = matrix.length
+        const matrixY = matrix[0].length
+        //Bresenham's line algorithm taken from SO: https://stackoverflow.com/questions/4672279/bresenham-algorithm-in-javascript
+        var x0 = this.from.x
+        var y0 = this.from.y
+        var x1 = this.to.x
+        var y1 = this.to.y
+        
+        const dx = abs (x1 - x0);
+        const dy = abs (y1 - y0);
+        const sx = sign(x1 - x0);
+        const sy = sign(y1 - y0);
+        let err = dx - dy;  
+        while (true) {
+            //scale x0 y0 to matrix coords
+            var mx = Math.ceil(x0*matrixW/100)-1
+            var my = Math.ceil(y0*matrixY/100)-1
+            brush.paint(matrix, mx, my)
+
+            if (x0 === x1 && y0 === y1) break;    
+            const e2 = 2 * err;
+            if (e2 > -dy) { err -= dy; x0 += sx; }
+            if (e2 <  dx) { err += dx; y0 += sy; }
+        }
+
+    }
+}
+
+
+/* code from chatgpt for bezier curve 
+
+function rasterizeBezierCurve(gridWidth, gridHeight, points, steps = 100) {
+    const cells = [];
+
+    // Helper function to evaluate a cubic Bézier curve
+    function evaluateBezier(t, p0, p1, p2, p3) {
+        const u = 1 - t;
+        return [
+            u ** 3 * p0[0] + 3 * u ** 2 * t * p1[0] + 3 * u * t ** 2 * p2[0] + t ** 3 * p3[0],
+            u ** 3 * p0[1] + 3 * u ** 2 * t * p1[1] + 3 * u * t ** 2 * p2[1] + t ** 3 * p3[1],
+        ];
+    }
+
+    // Sample the Bézier curve
+    const sampledPoints = [];
+    for (let i = 0; i <= steps; i++) {
+        const t = i / steps;
+        sampledPoints.push(evaluateBezier(t, ...points));
+    }
+
+    // Map sampled points to the grid resolution
+    const gridPoints = sampledPoints.map(([x, y]) => [
+        Math.round(x * gridWidth),
+        Math.round(y * gridHeight),
+    ]);
+
+    // Rasterize each line segment between sampled points
+    for (let i = 0; i < gridPoints.length - 1; i++) {
+        const [x0, y0] = gridPoints[i];
+        const [x1, y1] = gridPoints[i + 1];
+        cells.push(...bresenhamLine(x0, y0, x1, y1)); // Use Bresenham's Line Algorithm
+    }
+
+    return Array.from(new Set(cells.map(JSON.stringify))).map(JSON.parse); // Remove duplicates
+}
+
+// Example Bresenham's Line Algorithm (from before)
+function bresenhamLine(x0, y0, x1, y1) {
+    const cells = [];
+    const dx = Math.abs(x1 - x0);
+    const dy = Math.abs(y1 - y0);
+    const sx = x0 < x1 ? 1 : -1;
+    const sy = y0 < y1 ? 1 : -1;
+    let err = dx - dy;
+
+    while (true) {
+        cells.push([x0, y0]); // Add current grid cell
+        if (x0 === x1 && y0 === y1) break; // Reached endpoint
+        const e2 = 2 * err;
+        if (e2 > -dy) {
+            err -= dy;
+            x0 += sx;
+        }
+        if (e2 < dx) {
+            err += dx;
+            y0 += sy;
+        }
+    }
+
+    return cells;
+}
+
+// Example Usage
+const gridWidth = 10;
+const gridHeight = 10;
+
+// Define control points for the cubic Bézier curve
+const points = [
+    [0.1, 0.1], // P0
+    [0.3, 0.7], // P1
+    [0.7, 0.3], // P2
+    [0.9, 0.9], // P3
+];
+
+const bezierCells = rasterizeBezierCurve(gridWidth, gridHeight, points);
+console.log(bezierCells);
+
+
+*/
